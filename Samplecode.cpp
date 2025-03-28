6edb03688e93c59d7b914da72f1ae2028bdc8046
Abigail Rodriguez Vazquez <156958498+RodriguezVaz-Dev@users.noreply.github.com>
2025-03-27 22:51:32 -0500

    Add files via upload
    Im making a change yay

---
+++ Samplecode.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+std::string getuserInput;
+
+std::cout << "This is just a sample code say something cool !!";
+
+std::cin >> getuserInput;
+
+std::cout << "Wow your message : " << getuserInput << "\n is so cool !! \n";
+
+}
